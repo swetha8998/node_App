d658a3916e1e1ad32a11f825445758b266eb305a
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-02-26 15:26:51 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -6,6 +6,18 @@ stage( "build" ){
  sh 'echo "compressing the files present " '
 sh 'tar -cvf nodeapp.tar package.json server.js'
 }
+ stage( "initializing terraform"){
+  sh 'terraform init'
+ }
+ stage( "validating the configuration files"){
+  sh 'terraform validate'
+ }
+ stage( "terraform planning"){
+  sh 'terraform plan')
+ }
+ stage( "terraform apply"){
+  sh 'terraform apply'
+ }
 stage ( "approve"){
 sh 'echo "in approval stage" '
 
