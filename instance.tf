fd99f2e284380e34f9b836f6866d62fbc7f7445e
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-02-26 17:09:38 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -6,6 +6,29 @@ resource "aws_instance" "example"{
 ami = "ami-07f5c641c23596eb9"
 instance_type ="t2.micro"
 key_name = "${aws_key_pair.mykey.key_name}"
+  
+ provisioner "file" {
+    source      = "script.sh"
+    destination = "/tmp/script.sh"
+  }
+  provisioner "file" {
+    source ="package.json"
+    destination ="/tmp/package.json"
+    }
+  provisioner "file" {
+    source = "server.js"
+    destination ="/tmp/server.js"
+    }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "chmod +x /tmp/package.json",
+      "chmod +x /tmp/server.js",
+      "sudo sed -i -e 's/\r$//' /tmp/script.sh",  # Remove the spurious CR characters.
+      "sudo /tmp/script.sh",
+    ]
+  }
+  
 connection{
 user = "${var.INSTANCE_USERNAME}"
 private_key =file(var.PATH_TO_PRIVATE_KEY)
